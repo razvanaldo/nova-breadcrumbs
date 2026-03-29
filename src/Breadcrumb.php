cde31173d86266c3c3b63ae965771d27a7d52dc2
Dumbrava Razvan Aurel <razvanaldo@gmail.com>
2026-03-29 13:48:14 +0300

    Fix Breadcrumb::resource() signature for Nova 5 compatibility

--- src/Breadcrumb.php
+++ src/Breadcrumb.php
@@ -35,7 +35,7 @@ class Breadcrumb extends NovaBreadcrumb {
      * @return static
      */
 
-    public static function resource($resourceClass) {
+    public static function resource(Resource|string $resourceClass): static {
         if ($resourceClass instanceof Resource && $resourceClass->model()->exists === true) {
             return static::detailResource($resourceClass);
         }
